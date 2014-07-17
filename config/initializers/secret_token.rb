c79b1c3e121ce0aa44b50e06a54fd76b29c77e86
Zack Fisch <zack@zfisch.com>
2014-07-17 09:25:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlixTdd::Application.config.secret_key_base = '20b711001ae8cdd5729f365a6ffacc61546fb8fc821495b557992358043a77d1858d4dad9e18dcaa627b5da786623d3b531edf0a8115727404860cc2f8176324'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlixTdd::Application.config.session_store :cookie_store, key: '_flix-tdd_session'
